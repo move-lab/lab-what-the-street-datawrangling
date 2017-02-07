e220d501c261fbe1a79e3a6fed6a85a4a5a6d9c3
Stephan Bogner <hello@stephanbogner.de>
2017-02-07 22:24:19 +0100

    added script to add area information to mongodb

---
+++ 10_add_area/README.md
@@ -0,0 +1,12 @@
+# Add Area
+## About
+Add the individual way areas and adds them to the streets/rails.
+
+## Setup
+1. Navigate to folder (using the Terminal)
+2. Run `npm install`
+
+## Run
+1. Navigate to folder (using the Terminal)
+2. Run `node index.js`
+3. The script will tell you the details
---
+++ 10_add_area/index.js
@@ -0,0 +1,117 @@
+var fs = require('fs');
+var argv = require('minimist')(process.argv.slice(2));
+var async = require('async');
+var mongodb = require('mongodb');
+
+var MongoClient = mongodb.MongoClient;
+var collectionName;
+var waysCollectionName;
+
+main();
+
+function main() {
+    printInstructions();
+    if (argv.mongodb && argv.collection) {
+        collectionName = argv.collection;
+        waysCollectionName = argv.ways || 'ways';
+
+        printActiveSettings();
+        printProgressStart();
+        console.log();
+        console.log('   1. Iterating over Collection');
+        iterate(argv.mongodb);
+    }
+}
+
+function iterate(mongoUrl) {
+    stdout('      Connecting to mongodb ' + mongoUrl);
+    MongoClient.connect(mongoUrl, function(err, db) {
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+            stdout('      Connection successful');
+            var collection = db.collection(collectionName);
+            var waysCollection = db.collection(waysCollectionName);
+            var cursor = collection.find({});
+
+            cursor.once('end', function() {
+                stdout('      Done');
+                db.close();
+                printSummary();
+            });
+
+            cursor.on('data', function(doc) {
+                cursor.pause();
+                var counter = 0;
+                var areaCounter = 0;
+
+                async.eachSeries(doc.ways, function iteratee(way, callback) {
+                    counter ++;
+                    waysCollection.findOne({ '_id': way }, function(err, waysCursor) {
+                        if (waysCursor.properties_derived && waysCursor.properties_derived.area) {
+                            areaCounter += waysCursor.properties_derived.area;
+                        }
+                        setImmediate(callback);
+                    });
+
+                }, function() {
+                    stdout('      ' + doc.tags.name + ' (area = ' + areaCounter + ' m2)');
+                    doc.tags.area = areaCounter;
+                    collection.update({ _id: doc["_id"] }, doc);
+                    cursor.resume();
+                });
+            });
+        }
+    });
+}
+
+function printInstructions() {
+    console.log('');
+    console.log('--------------');
+    console.log('  HOW TO RUN  ');
+    console.log('--------------');
+    console.log('');
+    console.log('   Example:');
+    console.log('   node index.js --mongodb mongodb://username:password@ip:port/db?authSource=admin --collection ways');
+    console.log('');
+    console.log('   --mongodb: The connection to the mongoDB as url. E.g.: mongodb://username:password@ip:port/db?authSource=admin');
+    console.log('   --collection: [required] The name of the collection you want to connect to');
+    console.log('   --ways: [optional] The name of the ways collection containing the area information - defaults to \'ways\'');
+    console.log();
+}
+
+function stdout(str) {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(str);
+}
+
+function stdoutappend(str) {
+    process.stdout.write(str);
+}
+
+function printActiveSettings() {
+    console.log('--------------');
+    console.log('   SETTINGS      ');
+    console.log('--------------');
+    console.log();
+    console.log('   - mongodb: ' + argv.mongodb);
+    console.log('   - collection: ' + collectionName);
+    console.log('   - ways collection: ' + waysCollectionName);
+}
+
+function printProgressStart() {
+    console.log();
+    console.log('--------------');
+    console.log('   PROGRESS      ');
+    console.log('--------------');
+}
+
+function printSummary() {
+    console.log();
+    console.log();
+    console.log('--------------');
+    console.log('   COMPLETE      ');
+    console.log('--------------');
+    console.log();
+}
