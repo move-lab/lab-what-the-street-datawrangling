6cd9b096ec4ce76e4cc2545cd44db837d9ebda15
Stephan Bogner <hello@stephanbogner.de>
2017-06-14 01:02:36 +0200

    count area upwards as separate tag

--- 09_add_parking_space_size/index.js
+++ 09_add_parking_space_size/index.js
@@ -17,6 +17,8 @@ var parkingSpaceAreaCounter = 0;
 var parkingSpaceCounter = 0;
 var notFound = [];
 var defaultNumberOfBikeParking = 2; // when no capacity is given
+var addAreaCounter;
+var addAreaCounterDefault = 1;
 
 main();
 
@@ -25,6 +27,7 @@ function main() {
         var mongoUrl = argv.mongodb;
         mongoUrlRaw = mongoUrl.replace('_derived', '_raw');
         precision = argv.precision || defaultPrecision;
+        addAreaCounter = argv.count || addAreaCounterDefault;
         console.log();
         console.log('   1. Connecting to mongodb (' + mongoUrl + ')');
         MongoClient.connect(mongoUrl, function(err, db_derived) {
@@ -66,7 +69,6 @@ function alteringSvg(svgFile, db_derived, db_raw) {
                 stdout('      Iterating over polygons');
                 var output = result;
                 var polygons = result.svg.polygon;
-                //var newPolygons = [];
 
                 async.eachSeries(polygons, function iteratee(polygon, callback) {
                         parkingSpaceCounter++;
@@ -98,12 +100,18 @@ function alteringSvg(svgFile, db_derived, db_raw) {
                                                 parkingSpaceAreaCounter += area;
                                                 var roundedArea = roundToDecimals(area, precision);
                                                 polygonContent.moovel_area = roundedArea;
+                                                if (addAreaCounter === 1) {
+                                                    polygonContent.moovel_cumulative_area = parkingSpaceAreaCounter.toFixed(2);
+                                                }
                                                 stdoutappend(' - ' + roundedArea);
                                             } else {
                                                 var area = document.tags.capacity.capacity * 1.6; // m^2 (bikeparkw = 0.8, bikeparkh = 2)
                                                 parkingSpaceAreaCounter += area;
                                                 var roundedArea = roundToDecimals(area, precision);
                                                 polygonContent.moovel_area = roundedArea;
+                                                if (addAreaCounter === 1) {
+                                                    polygonContent.moovel_cumulative_area = parkingSpaceAreaCounter.toFixed(2);
+                                                }
                                                 stdoutappend(' - ' + roundedArea);
                                             }
                                         }
@@ -122,6 +130,9 @@ function alteringSvg(svgFile, db_derived, db_raw) {
                                     var roundedArea = roundToDecimals(area, precision);
                                     stdoutappend(' - ' + roundedArea);
                                     polygonContent.moovel_area = roundedArea;
+                                    if (addAreaCounter === 1) {
+                                        polygonContent.moovel_cumulative_area = parkingSpaceAreaCounter.toFixed(2);
+                                    }
                                 }
                                 setImmediate(callback);
                             }
@@ -196,11 +207,12 @@ function printInstructions() {
     console.log('--------------');
     console.log('');
     console.log('For svg:');
-    console.log("  node index.js --svg yourSvgFile.svg --mongodb mongodb://username:password@ip:port/db?authSource=admin --precision 2");
+    console.log("  node index.js --svg yourSvgFile.svg --mongodb mongodb://127.0.0.1:27017/berlin_derived");
     console.log('');
     console.log("  - svg:           The svg file you want to be altered.");
     console.log("  - mongodb:       The connection to the mongoDB as url. E.g.: mongodb://username:password@ip:port/db?authSource=admin");
     console.log("  - precision:    The number of decimals the area should be copied with - defaults to " + defaultPrecision);
+    console.log("  - count:    [1/0] If every parking space tag should contain the accumulated area of all previous parking spots - defaults to " + addAreaCounterDefault);
     console.log('');
     console.log('Exiting now!');
     console.log('');
